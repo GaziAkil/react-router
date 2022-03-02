334afc0372533d27bc9117f6e609ee7d5143c6d7
GaziAkil <gazi.akil@sjinnovation.com>
2022-03-02 15:53:53 +0600

    Error message updated

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Error() {
   return (
     <div>
-      <h1>404: Page not found!</h1>
+      <h1 className="display-5 text-center mark">404: Page not found!</h1>
     </div>
   )
 }
